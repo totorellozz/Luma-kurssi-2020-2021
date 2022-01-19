c162bf8350e37af786e4d67046c5acf51f7707d6
Tommi Mosconi <tommi.mosconi@gmail.com>
2022-01-19 09:57:58 +0200

    uusia tehtäviä/korjauksia

---
+++ Muuttujat ja ehtolauseke tehtävä 2.py
@@ -0,0 +1,6 @@
+password = "luma2022"
+salasana = input("Kirjoita salasana: ")
+if salasana == password:
+    print("salasana on oikein")
+else:
+    print("Salasana on väärin")
---
+++ Muuttujat ja ehtolauseke tehtävä 3.py
@@ -0,0 +1,23 @@
+print("Tervetuloa tietokilpailuun!")
+vastaus1 = "kyllä"
+vastaus2 = "kyllä"
+vastaus3 = "ei"
+
+vastaus = input("Onko lehmät eläimiä?: ")
+if vastaus == vastaus1:
+    print("Oikein!")
+else:
+    print("Väärin!")
+
+
+vastaus = input("Onko kissat eläimiä?: ")
+if vastaus == vastaus2:
+    print("Oikein!")
+else:
+    print("Väärin!")
+
+vastaus = input("Onko sammakot ihmisiä: ")
+if vastaus == vastaus3:
+    print("Oikein!")
+else:
+    print("Väärin!")
